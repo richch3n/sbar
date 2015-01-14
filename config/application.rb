d47b757e614c08ad3bceec863e1b083fb3cdacc5
rich <richch3n@gmail.com>
2015-01-13 21:04:05 -0800

    added devise

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Sbar
   class Application < Rails::Application
+
+    config.assets.initialize_on_compile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
